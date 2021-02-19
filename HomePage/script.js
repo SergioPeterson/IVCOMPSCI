d0f6eee3e854518eb98a06cdf5f605d23d424493
Sergio W. Peterson <Sergiopeter2016@berkeley.edu>
2021-02-18 21:12:05 -0800

    Fixed main page

--- HomePage/script.js
+++ HomePage/script.js
@@ -1,19 +1,24 @@
 const workshopDate = new Date("March 20, 2021 11:00:00").getTime();
-const deadlineDate = new Date("March 20, 2021 11:00:00").getTime();
-const ceremonyDate = new Date("March 20, 2021 11:00:00").getTime();
+const deadlineDate = new Date("April 25, 2021 23:59:59").getTime();
+const ceremonyDate = new Date("May 01, 2021 09:00:00").getTime();
 
 
 var x = setInterval(function(){
-    
-})
+    countdown(workshopDate, "workshopTimer");
+    countdown(deadlineDate, "deadlineTimer");
+    countdown(ceremonyDate, "ceremonyTimer");
+}, 1000);
 
-function countdown(date){
+function countdown(date, place){
     var now = new Date().getTime();
     var distance = date-now;
     var days = Math.floor(distance / (1000 * 60 * 60 * 24));
     var hours = Math.floor((distance % (1000 * 60 * 60 * 24)) / (1000 * 60 * 60));
     var minutes = Math.floor((distance % (1000 * 60 * 60)) / (1000 * 60));
     var seconds = Math.floor((distance % (1000 * 60)) / 1000);
-     
-
+    document.getElementById(place).innerHTML = days + ":" + hours + ":" + minutes + ":" + seconds;
+    if(distance < 0){
+        clearInterval(x);
+        document.getElementById(date).innerHTML = "EXPIRED";
+    }
 }
